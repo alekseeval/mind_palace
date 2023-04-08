9c7751ab66ec38f4211ba0d369756c22200002b5
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-04-08 17:42:51 +0300

    Ideas table commented for the future

--- db/create_scheme.sql
+++ db/create_scheme.sql
@@ -5,33 +5,36 @@ ALTER ROLE CURRENT_ROLE SET SEARCH_PATH to mind_palace, mind_palace_api;
 
 CREATE TABLE if not exists mind_palace.users (
     id serial PRIMARY KEY,
+    name VARCHAR,
     tg_id VARCHAR UNIQUE
 );
 
 CREATE TABLE if not exists mind_palace.themes (
     id serial PRIMARY KEY,
     name VARCHAR NOT NULL,
+    main_theme_id int,
     user_id int,
 
-    FOREIGN KEY (user_id) REFERENCES mind_palace.users (id)
+    FOREIGN KEY (user_id) REFERENCES mind_palace.users (id),
+    FOREIGN KEY (main_theme_id) REFERENCES mind_palace.themes (id)
 );
 
-CREATE TABLE if not exists mind_palace.ideas (
-    id serial PRIMARY KEY,
-    text VARCHAR,
-    theme_id int,
-    user_id int,
-
-    FOREIGN KEY (theme_id) REFERENCES mind_palace.themes (id),
-    FOREIGN KEY (user_id) REFERENCES mind_palace.users (id)
-);
+-- CREATE TABLE if not exists mind_palace.ideas (
+--     id serial PRIMARY KEY,
+--     text VARCHAR,
+--     theme_id int,
+--     user_id int,
+--
+--     FOREIGN KEY (theme_id) REFERENCES mind_palace.themes (id),
+--     FOREIGN KEY (user_id) REFERENCES mind_palace.users (id)
+-- );
 
 CREATE TABLE if not exists mind_palace.questions (
     id serial PRIMARY KEY,
-    text VARCHAR,
+    title VARCHAR,
     main_idea int,
     theme_id int,
-    number_of_correct_answers int,
+    correct_answered_time int,
     user_id int,
 
     FOREIGN KEY (theme_id) REFERENCES mind_palace.themes (id),
