7e6688c1b0f04289e15cbb064b70842705beea7a
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-04-10 01:03:03 +0300

    Back to ideas...

--- db/create_scheme.sql
+++ db/create_scheme.sql
@@ -1,5 +1,4 @@
 create schema mind_palace;
-create schema mind_palace_api;
 
 ALTER ROLE CURRENT_ROLE SET SEARCH_PATH to mind_palace, mind_palace_api;
 
@@ -11,7 +10,8 @@ CREATE TABLE if not exists mind_palace.users (
 
 CREATE TABLE if not exists mind_palace.themes (
     id serial PRIMARY KEY,
-    name VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+
     main_theme_id int,
     user_id int,
 
@@ -19,25 +19,27 @@ CREATE TABLE if not exists mind_palace.themes (
     FOREIGN KEY (main_theme_id) REFERENCES mind_palace.themes (id)
 );
 
--- CREATE TABLE if not exists mind_palace.ideas (
---     id serial PRIMARY KEY,
---     text VARCHAR,
---     theme_id int,
---     user_id int,
---
---     FOREIGN KEY (theme_id) REFERENCES mind_palace.themes (id),
---     FOREIGN KEY (user_id) REFERENCES mind_palace.users (id)
--- );
-
 CREATE TABLE if not exists mind_palace.questions (
     id serial PRIMARY KEY,
     title VARCHAR,
-    main_idea int,
     theme_id int,
     correct_answered_time int,
+
     user_id int,
 
     FOREIGN KEY (theme_id) REFERENCES mind_palace.themes (id),
-    FOREIGN KEY (main_idea) REFERENCES mind_palace.ideas (id),
+    FOREIGN KEY (user_id) REFERENCES mind_palace.users (id)
+);
+
+CREATE TABLE if not exists mind_palace.ideas (
+    id serial PRIMARY KEY,
+    title VARCHAR,
+    text VARCHAR,
+
+    is_answer boolean,
+    question_id int,
+    user_id int,
+
+    FOREIGN KEY (question_id) REFERENCES mind_palace.themes (id),
     FOREIGN KEY (user_id) REFERENCES mind_palace.users (id)
 );
