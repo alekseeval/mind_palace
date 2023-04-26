687c49ea862fc98a9f1c7bb5a91eeb5360a52160
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-04-27 00:43:35 +0300

    Fix returning query

--- db/db_api/getThemes.sql
+++ db/db_api/getThemes.sql
@@ -1,16 +1,10 @@
 CREATE OR REPLACE FUNCTION mind_palace_api.get_all_themes_for_user (p_user_id int)
-RETURNS mind_palace.themes
+RETURNS SETOF mind_palace.themes
 LANGUAGE plpgsql
 AS
 $$
-Declare
-    r_themes mind_palace.themes;
 BEGIN
-    SELECT *
-    INTO r_themes
-    FROM mind_palace.themes
-    WHERE user_id=p_user_id;
-
-    return r_themes;
+    return query
+        SELECT * FROM mind_palace.themes WHERE user_id=p_user_id;
 END;
 $$;
