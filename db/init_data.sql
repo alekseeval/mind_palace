97e82d079e66102a9ee92f4a279b44db12d7b270
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-04-26 21:07:19 +0300

    Changed users init data. Increment seq then first run

--- db/init_data.sql
+++ db/init_data.sql
@@ -4,6 +4,18 @@ INSERT INTO mind_palace.note_types(id, title) VALUES (1, 'note'),
 ON CONFLICT (id) DO UPDATE
     SET title = excluded.title;
 
-INSERT INTO mind_palace.users(id, name, tg_id) VALUES (1, 'system', null)
-ON CONFLICT (id) DO UPDATE
-    SET name = excluded.name;
+DO
+$$
+DECLARE
+    users_count int;
+BEGIN
+    SELECT count(*) INTO users_count from mind_palace.users;
+    if users_count = 0 then
+        PERFORM nextval('mind_palace.users_id_seq');
+    end if;
+    INSERT INTO mind_palace.users(id, name, tg_id) VALUES (1, 'system', null)
+    ON CONFLICT (id) DO UPDATE
+        SET name = excluded.name;
+
+END
+$$;
