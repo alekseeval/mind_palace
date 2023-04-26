41131ebd0c4e746fa7ceda0013e243bade26d204
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-04-26 19:34:51 +0300

    Added system user

--- db/init_data.sql
+++ db/init_data.sql
@@ -3,3 +3,7 @@ INSERT INTO mind_palace.note_types(id, title) VALUES (1, 'note'),
                                                      (3, 'task')
 ON CONFLICT (id) DO UPDATE
     SET title = excluded.title;
+
+INSERT INTO mind_palace.users(id, name, tg_id) VALUES (1, 'system', null)
+ON CONFLICT (id) DO UPDATE
+    SET name = excluded.name;
