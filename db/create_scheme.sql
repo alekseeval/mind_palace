e22408771fe555241f3f5885d6adafab7dbc68db
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-04-26 16:32:17 +0300

    Add unique constraints

--- db/create_scheme.sql
+++ db/create_scheme.sql
@@ -6,34 +6,38 @@ ALTER ROLE CURRENT_ROLE SET SEARCH_PATH to mind_palace, mind_palace_api;
 CREATE TABLE if not exists mind_palace.users (
     id serial PRIMARY KEY,
     name VARCHAR,
-    tg_id VARCHAR UNIQUE
+    tg_id VARCHAR,
+
+    UNIQUE(tg_id)
 );
 
 CREATE TABLE if not exists mind_palace.themes (
     id serial PRIMARY KEY,
     title VARCHAR NOT NULL,
-
     main_theme_id int,
     user_id int,
 
+    UNIQUE(title, user_id),
     FOREIGN KEY (user_id) REFERENCES mind_palace.users (id),
     FOREIGN KEY (main_theme_id) REFERENCES mind_palace.themes (id)
 );
 
 CREATE TABLE if not exists mind_palace.note_types (
     id serial PRIMARY KEY,
-    title VARCHAR
+    title VARCHAR,
+
+    UNIQUE(title)
 );
 
 CREATE TABLE if not exists mind_palace.notes (
     id serial PRIMARY KEY,
     title VARCHAR,
     text VARCHAR,
-
     note_type int,
     theme_id int,
     user_id int,
 
+    UNIQUE(title, user_id),
     FOREIGN KEY (theme_id) REFERENCES mind_palace.themes (id),
     FOREIGN KEY (note_type) REFERENCES mind_palace.note_types (id),
     FOREIGN KEY (user_id) REFERENCES mind_palace.users (id)
