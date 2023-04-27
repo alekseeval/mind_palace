8d491d747b1e2adc5b179dbb5ff9bd55f879d435
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-04-27 23:28:43 +0300

    Upgrade way to set init user

--- db/init_data.sql
+++ db/init_data.sql
@@ -6,11 +6,8 @@ ON CONFLICT (id) DO UPDATE
 
 DO
 $$
-DECLARE
-    users_count int;
 BEGIN
-    SELECT count(*) INTO users_count from mind_palace.users;
-    if users_count = 0 then
+    if NOT EXISTS (SELECT * from mind_palace.users) then
         PERFORM nextval('mind_palace.users_id_seq');
     end if;
     INSERT INTO mind_palace.users(id, name, tg_id) VALUES (1, 'system', null)
