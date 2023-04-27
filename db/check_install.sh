892cafc87b94908836de34494236118223bf7236
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-04-27 20:25:37 +0300

    logs: successfully

--- db/check_install.sh
+++ db/check_install.sh
@@ -27,19 +27,19 @@ export PGPASSWORD="$POSTGRES_PASSWORD"
 psql -h "$HOST" -p "$PORT" --user "$POSTGRES" --dbname "postgres" -c "CREATE DATABASE $DB_NAME;"
 psql -h "$HOST" -p "$PORT" --user "$POSTGRES" --dbname "postgres" -c "create user $DB_ADMIN with encrypted password '$DB_ADMIN_DB_ADMIN_PASSWORD';"
 psql -h "$HOST" -p "$PORT" --user "$POSTGRES" --dbname "postgres" -c "grant all privileges on database $DB_NAME to $DB_ADMIN;"
-echo -e "---> DB $DB_NAME was created\n"
+echo -e "---> DB $DB_NAME was successfully created\n"
 
 # Installation DB scheme from create_scheme.sql
 echo "---> Start DB scheme creation..."
 PGPASSWORD="$DB_ADMIN_PASSWORD"
 psql -h "$HOST" -p "$PORT" --user "$DB_ADMIN" --dbname "$DB_NAME" < create_scheme.sql
-echo -e "---> DB scheme was created\n"
+echo -e "---> DB scheme was successfully created\n"
 
 # Create init DB data by init_data.sql
 echo "---> Fill DB with init data..."
 PGPASSWORD="$DB_ADMIN_PASSWORD"
 psql -h "$HOST" -p "$PORT" --user "$DB_ADMIN" --dbname "$DB_NAME" < init_data.sql
-echo -e "---> Init data was inserted\n"
+echo -e "---> Init data was successfully inserted\n"
 
 # Installation DB api functions from /db_api directory
 echo "---> Installing DB API..."
