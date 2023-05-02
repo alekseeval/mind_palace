d0770918d90aaf42876674a43c4ac8e0f6023e5f
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-05-02 23:26:16 +0300

    Db initalization

--- internal/mindPalace/dal/postgre.go
+++ internal/mindPalace/dal/postgre.go
@@ -2,6 +2,7 @@ package dal
 
 import (
 	"MindPalace/internal/mindPalace/configuration"
+	"fmt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
@@ -12,8 +13,17 @@ type PostgresDB struct {
 	db *sqlx.DB
 }
 
+// TODO: Additional dbconn settings (maxconns, timeout)
 func NewPostgresDB(config *configuration.Config) (*PostgresDB, error) {
+	dbConfig := config.System.DB
+	connStr := fmt.Sprintf("user=%s password=%s host=%s port=%d dbname=%s",
+		dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.DBName)
+	db, err := sqlx.Connect("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
 	return &PostgresDB{
-		db: &sqlx.DB{},
-	}, nil
+		db: db,
+	}, err
 }
