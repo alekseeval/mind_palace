a5bac8dfaf7b655961b2dd991a4b5d1e356cafb4
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-05-02 22:13:41 +0300

    Added config read

--- cmd/mpapp/mpapp.go
+++ cmd/mpapp/mpapp.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"MindPalace/internal/mindPalace/configuration"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"log"
+)
+
+var PATH_TO_CONFIG string = "/home/reserv/GolandProjects/MindPalace/internal/mindPalace/config.yaml"
 
 func main() {
-	fmt.Println("Hello world!")
+	config, err := configuration.ReadConfig(PATH_TO_CONFIG)
+	if err != nil {
+		log.Fatal("Error occurred when read config file", err)
+	}
+	fmt.Println(config)
+}
+
+func initPostgresDb() (*sqlx.DB, error) {
+	return nil, nil
 }
--- internal/mindPalace/configuration/config.go
+++ internal/mindPalace/configuration/config.go
@@ -1,1 +1,35 @@
 package configuration
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	System struct {
+		Http struct {
+			Host string `mapstructure:"host"`
+			Port int    `mapstructure:"port"`
+		}
+		Grpc struct {
+			Port int `mapstructure:"port"`
+		}
+		DB struct {
+			Host    string `mapstructure:"host"`
+			Port    int    `mapstructure:"port"`
+			Name    string `mapstructure:"name"`
+			MaxConn int    `mapstructure:"max_conn"`
+		}
+	}
+}
+
+func ReadConfig(path string) (config *Config, err error) {
+	viper.SetConfigFile(path)
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+	err = viper.Unmarshal(&config)
+	return config, err
+}
