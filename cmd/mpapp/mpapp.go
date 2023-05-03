923cab473b7c87c1a2f062f50d7b8c77ee60d6a7
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-05-04 00:10:30 +0300

    Test userDAO

--- cmd/mpapp/mpapp.go
+++ cmd/mpapp/mpapp.go
@@ -14,21 +14,50 @@ var PATH_TO_CONFIG string = "/home/reserv/GolandProjects/MindPalace/internal/min
 func main() {
 	config, err := configuration.ReadConfig(PATH_TO_CONFIG)
 	if err != nil {
-		log.Fatal("Error occurred when read config file", err)
+		log.Fatal("Error occurred when read config file\t", err)
 	}
 
 	dbDAO, err := dal.NewPostgresDB(config)
 	if err != nil {
-		log.Fatal("Failed to create connection to DB", err)
+		log.Fatal("Failed to create connection to DB\t", err)
 	}
-	user, err := dbDAO.SaveUser(model.User{
-		Name:       "Alekseev Andrey",
-		TelegramId: 123123123123,
+
+	var userTgId int64 = 123123
+	var user *model.User
+
+	// --------------------------------------------------------
+	var userName *string = nil
+	user, err = dbDAO.SaveUser(model.User{
+		Name:       userName,
+		TelegramId: &userTgId,
 	})
 	if err != nil {
-		log.Fatal("Failed to create user")
+		log.Fatal("Failed to create user\t", err)
 	}
 	fmt.Println(user)
+
+	// --------------------------------------------------------
+	user, err = dbDAO.GetUserByTgId(userTgId)
+	if err != nil {
+		log.Fatal("Failed to GET user from DB\t", err)
+	}
+	fmt.Println(user)
+
+	// --------------------------------------------------------
+	newUserName := "Alekseev Andrey"
+	user.Name = &newUserName
+	user, err = dbDAO.ChangeUser(user)
+	if err != nil {
+		log.Fatal("Failed to change user in DB\t", err)
+	}
+	fmt.Println(user)
+
+	// --------------------------------------------------------
+	id, err := dbDAO.DeleteUser(user.Id)
+	if err != nil {
+		log.Fatal("Failed to delete user from DB\t", err)
+	}
+	fmt.Println("User was deleted ", id)
 }
 
 func initPostgresDb() (*sqlx.DB, error) {
--- db/db_api/deleteUser.sql
+++ db/db_api/deleteUser.sql
@@ -1,12 +1,12 @@
 CREATE OR REPLACE FUNCTION mind_palace_api.delete_user (p_user_id int)
-RETURNS int
+RETURNS mind_palace.users
 LANGUAGE plpgsql
 AS
 $$
 DECLARE
-    r_id int;
+    r_user mind_palace.users;
 BEGIN
-    DELETE FROM mind_palace.users WHERE id=p_user_id RETURNING id INTO r_id;
-    RETURN r_id;
+    DELETE FROM mind_palace.users WHERE id=p_user_id RETURNING * INTO r_user;
+    RETURN r_user;
 END;
 $$;
