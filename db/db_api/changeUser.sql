f917998c121962a995a2f50210c523bb599dbc74
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-05-03 23:53:56 +0300

    Now function return all fields of chenged user

--- db/db_api/changeUser.sql
+++ db/db_api/changeUser.sql
@@ -1,11 +1,10 @@
 CREATE OR REPLACE FUNCTION mind_palace_api.change_user (p_id int, p_name varchar = null, p_tg_id bigint = null)
-RETURNS int
+RETURNS mind_palace.users
 LANGUAGE plpgsql
 AS
 $$
 DECLARE
     cur_user mind_palace.users;
-    r_id int;
 BEGIN
     SELECT * INTO cur_user
     FROM mind_palace.users
@@ -21,8 +20,8 @@ BEGIN
     UPDATE mind_palace.users
     SET name=p_name, tg_id=p_tg_id
     WHERE id=p_id
-    RETURNING id INTO r_id;
+    RETURNING * INTO cur_user;
 
-    RETURN r_id;
+    RETURN cur_user;
 END;
 $$;
