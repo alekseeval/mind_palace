26f570fa42bb838751825b74d7b2ae6da12f3125
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-05-04 23:37:55 +0300

    Now function return all fields of changed object

--- db/db_api/changeTheme.sql
+++ db/db_api/changeTheme.sql
@@ -1,10 +1,9 @@
 CREATE OR REPLACE FUNCTION mind_palace_api.change_theme (p_theme_id int, p_title varchar = null, p_main_theme_id int = null)
-RETURNS int
+RETURNS mind_palace.themes
 LANGUAGE plpgsql
 AS
 $$
 DECLARE
-    r_id int;
     changed_theme mind_palace.themes;
 BEGIN
     SELECT * INTO changed_theme FROM mind_palace.themes WHERE id=p_theme_id;
@@ -17,8 +16,8 @@ BEGIN
     if p_main_theme_id = 0 then
         p_main_theme_id = null;
     end if;
-    UPDATE mind_palace.themes SET title=p_title, main_theme_id=p_main_theme_id WHERE id=p_theme_id RETURNING id INTO r_id;
+    UPDATE mind_palace.themes SET title=p_title, main_theme_id=p_main_theme_id WHERE id=p_theme_id RETURNING * INTO changed_theme;
 
-    RETURN r_id;
+    RETURN changed_theme;
 END;
 $$;
