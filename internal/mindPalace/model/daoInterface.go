249a7aa59f4b68a96a9a1dedf63d22f43c0073d8
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-05-08 00:04:54 +0300

    Changed interfaces naming

--- internal/mindPalace/model/daoInterface.go
+++ internal/mindPalace/model/daoInterface.go
@@ -1,26 +1,26 @@
 package model
 
-type DAO interface {
-	UserDAO
-	ThemeDAO
-	NoteDAO
+type IDAO interface {
+	IUserDAO
+	IThemeDAO
+	INoteDAO
 }
 
-type UserDAO interface {
+type IUserDAO interface {
 	SaveUser(user User) (*User, error)
 	GetUserByTgId(telegramIid int64) (*User, error)
 	ChangeUser(user *User) (*User, error)
 	DeleteUser(userId int) (int, error)
 }
 
-type ThemeDAO interface {
+type IThemeDAO interface {
 	CreateTheme(theme Theme) (*Theme, error)
 	GetAllUserThemes(userId int) ([]*Theme, error)
 	ChangeTheme(theme *Theme) (*Theme, error)
 	DeleteTheme(themeId int) (int, error)
 }
 
-type NoteDAO interface {
+type INoteDAO interface {
 	CreateNote(note Note) (*Note, error)
 	GetAllUserNotesByTheme(userId int, themeId int) ([]*Note, error)
 	ChangeNote(note *Note) (*Note, error)
