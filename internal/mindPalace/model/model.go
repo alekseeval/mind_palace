eccf3cb0c66a00a73f7454d02588bdf413551166
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-05-14 23:13:16 +0300

    Added theme update model

--- internal/mindPalace/model/model.go
+++ internal/mindPalace/model/model.go
@@ -15,8 +15,8 @@ type User struct {
 }
 
 type UserUpdate struct {
-	Name       *string `db:"name" json:"name"`
-	TelegramId *int64  `db:"tg_id" json:"tg_id"`
+	Name       *string `json:"name"`
+	TelegramId *int64  `json:"tg_id"`
 }
 
 func (u *UserUpdate) UpdateUser(user *User) *User {
@@ -34,6 +34,25 @@ type Theme struct {
 	Title       string `db:"title" json:"title"`
 	MainThemeId *int   `db:"main_theme_id" json:"main_theme_id"`
 	UserId      *int   `db:"user_id" json:"user_id"`
+}
+
+type ThemeUpdate struct {
+	Title       *string `json:"title"`
+	MainThemeId *int    `json:"main_theme_id"`
+	UserId      *int    `json:"user_id"`
+}
+
+func (tu *ThemeUpdate) UpdateTheme(theme *Theme) *Theme {
+	if tu.Title != nil {
+		theme.Title = *tu.Title
+	}
+	if tu.MainThemeId != nil {
+		theme.MainThemeId = tu.MainThemeId
+	}
+	if tu.UserId != nil {
+		theme.UserId = tu.UserId
+	}
+	return theme
 }
 
 type Note struct {
