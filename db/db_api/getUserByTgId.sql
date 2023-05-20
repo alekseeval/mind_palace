327a8eb64e24443657c68ac6522cb5ca4b9a6aab
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-05-21 00:21:25 +0300

    added exception

--- db/db_api/getUserByTgId.sql
+++ db/db_api/getUserByTgId.sql
@@ -5,8 +5,13 @@ AS
 $$
 DECLARE
     r_user mind_palace.users;
+    v_cnt int;
 BEGIN
     SELECT * INTO r_user FROM mind_palace.users WHERE tg_id=p_tg_id;
+    GET DIAGNOSTICS v_cnt := ROW_COUNT;
+    IF v_cnt = 0 THEN
+        RAISE EXCEPTION 'There is no user with tg id=%', p_tg_id;
+    END IF;
     RETURN r_user;
 END;
 $$;
