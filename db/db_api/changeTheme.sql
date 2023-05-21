f940e4e5806ca9cf5003fcd456592d395739c42d
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-05-22 00:12:18 +0300

    Return view of theme

--- db/db_api/changeTheme.sql
+++ db/db_api/changeTheme.sql
@@ -1,10 +1,11 @@
 CREATE OR REPLACE FUNCTION mind_palace_api.change_theme (p_theme_id int, p_title varchar, p_main_theme_id int)
-RETURNS mind_palace.themes
+RETURNS mind_palace.v_theme
 LANGUAGE plpgsql
 AS
 $$
 DECLARE
-    changed_theme mind_palace.themes;
+    changed_theme mind_palace.v_theme;
+    changed_theme_id int;
 BEGIN
     if p_theme_id is NULL then
         RAISE exception 'no theme id provided';
@@ -20,12 +21,12 @@ BEGIN
     UPDATE mind_palace.themes
     SET title=p_title, main_theme_id=p_main_theme_id
     WHERE id=p_theme_id
-    RETURNING * INTO changed_theme;
-
-    if changed_theme is NULL then
+    RETURNING id INTO changed_theme_id;
+    if changed_theme_id is NULL then
         RAISE EXCEPTION 'no such theme';
     end if;
 
+    SELECT * into changed_theme from v_theme where id=changed_theme_id;
     RETURN changed_theme;
 END;
 $$;
