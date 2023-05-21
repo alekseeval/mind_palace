b5ae0f0fa9ab6fcb33c7621afb8797a6532df206
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-05-21 23:27:49 +0300

    Fix return value

--- db/db_api/getThemesByUser.sql
+++ db/db_api/getThemesByUser.sql
@@ -9,10 +9,11 @@ BEGIN
     IF p_user_name is NULL then
         return query
             SELECT * FROM mind_palace.v_theme;
+        return;
     end if;
     select id into v_user_id from users where name=p_user_name;
     if v_user_id is null then
-        RAISE EXCEPTION 'no such user';
+        RAISE EXCEPTION 'no such user %', p_user_name;
     end if;
     return query
         SELECT * FROM mind_palace.v_theme WHERE user_name=p_user_name;
