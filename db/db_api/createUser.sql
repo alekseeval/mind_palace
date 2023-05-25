6fd3aab9dd718bbf1c8cbaa215b5999f0e168b57
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-05-25 22:53:10 +0300

    Added user name length check

--- db/db_api/createUser.sql
+++ db/db_api/createUser.sql
@@ -6,6 +6,9 @@ $$
 DECLARE
     r_user users;
 BEGIN
+    if length(p_name) > 30 then
+        RAISE SQLSTATE '80001' USING MESSAGE = 'user name too long';
+    end if;
     INSERT INTO mind_palace.users(name, tg_id) VALUES (p_name, p_tg_id) RETURNING * INTO r_user;
     RETURN r_user;
 END
